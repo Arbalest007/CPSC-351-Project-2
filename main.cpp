38b50b5e81edaa2bb7bc7c43286470617562d16e
Patrick Lin <51501192+Arbalest007@users.noreply.github.com>
2022-04-22 17:56:29 -0700

    Update main.cpp
    -Minor formatting

--- main.cpp
+++ main.cpp
@@ -222,7 +222,7 @@ int main(int argc, char* argv[]) {
     }
 
     cout << "The merged array is: " << endl;
-  for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         if(i + 1 == arr.size())
             cout << arr[i] << "\n\n";
         else
